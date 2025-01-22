1b13be044369442c47740dfdb6b177c8d143b73b
Denis (mr_dramm) <like-nix@mail.ru>
2025-01-22 22:41:05 +0300

    remove unused code

--- app/texts/page.tsx
+++ app/texts/page.tsx
@@ -1,44 +1,5 @@
-"use client";
-import React, { useTransition } from "react";
-import { getAllTexts, TextShort } from "@/db";
-
-const TextArea = () => {
-  
-  return <div contentEditable></div>
-}
-
-const List = () => {
-  const [texts, setTexts] = React.useState<TextShort[]>([]);
-  const [isPending, startTransition] = useTransition();
-
-  const fetchTexts = () => {
-    startTransition(async () => {
-      const data = await getAllTexts();
-      setTexts(data);
-    });
-  };
-
-
-  return (
-    <div>
-      <button onClick={fetchTexts} disabled={isPending}>
-        {isPending ? "Loading..." : "Fetch Texts"}
-      </button>
-      <div>
-        {texts.map((text) => (
-          <div key={text.id}>{JSON.stringify(text)}</div>
-        ))}
-      </div>
-    </div>
-  );
+const Page = () => {
+  return <div></div>;
 };
 
-const Texts = () => {
-  return (
-    <div>
-      <List />
-    </div>
-  );
-};
-
-export default Texts;
+export default Page;
