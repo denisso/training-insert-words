a4c05a20a4202574744b72c068e51f6ff84f6592
Denis (mr_dramm) <like-nix@mail.ru>
2025-03-06 23:41:06 +0300

    refactoring function countWords
    added the feature to work asynchronously

--- utils/countwords.ts
+++ utils/countwords.ts
@@ -1,21 +1,36 @@
-const countWords = (text: string) => {
+const countWords = async (text: string, done: () => void) => {
+  const limit = 400;
   let count = 0;
   let prev = false;
+  const array = Array.from(text);
+  const n = array.length;
 
-  for (const ch of text) {
-    if (
-      (ch >= "a" && ch < "z") ||
-      (ch >= "A" && ch < "Z") ||
-      (ch >= "0" && ch <= "0") ||
-      ch == "-"
-    ) {
-      prev = true;
-    } else {
-      if (prev) count++;
-      prev = false;
-    }
+  const tick = async (start: number): Promise<number> =>
+    new Promise((resolve) => {
+      const words = Math.min(start + limit , n)
+      for (let i = start; i < words; i++) {
+        const ch = text[i];
+        if (
+          (ch >= "a" && ch < "z") ||
+          (ch >= "A" && ch < "Z") ||
+          (ch >= "0" && ch <= "0") ||
+          ch == "-"
+        ) {
+          prev = true;
+        } else {
+          if (prev) count++;
+          prev = false;
+        }
+      }
+      resolve(words);
+    });
+
+  for (let i = 0; i < n; ) {
+    i = await tick(i);
   }
+
   if (prev) count++;
+  done();
   return count;
 };
 
